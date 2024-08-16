15ffaefbdb6fbe5ebf4ca4655990649eb3389834
troclove0113 <ntmobile747@gmail.com>
2024-08-16 18:06:58 +0700

    a

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Phong Tuan!");
+        System.out.println("Welcome to the DHKTPM19ATT!");
+    }
+}
